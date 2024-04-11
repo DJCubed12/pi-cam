295bbb78d44363b49c6d9617acddc227209d3bd0
Carson Jones <djcubed12@gmail.com>
2024-04-10 19:14:43 -0500

    Init

---
+++ apache-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Remove old html files, copy in new ones
+if [ -d /var/www/html ]; then
+  rm -r /var/www/html
+fi
+cp -r www/html /var/www
+chmod -R u=rwX,go=rX /var/www
+
+# Remove old cgi-bin files, copy in new ones
+if [ -d /usr/lib/cgi-bin ]; then
+  rm -r /usr/lib/cgi-bin
+fi
+cp -r www/cgi-bin /usr/lib
+chmod -R u=rwx,go=rx /usr/lib/cgi-bin
+
+systemctl start apache2
---
+++ www/cgi-bin/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Content-type: text/html"
+echo ""
+echo "Hello, World! This is Pi-Cam!"
+
+exit 0
