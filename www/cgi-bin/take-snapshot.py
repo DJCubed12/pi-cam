6120b2ffa0d8e8e7554a52e502f0626ec1ea870d
DJCubed12 <djcubed12@gmail.com>
2024-04-11 17:17:53 -0500

    take-snapshot.py returns json

--- www/cgi-bin/take-snapshot.py
+++ www/cgi-bin/take-snapshot.py
@@ -1,5 +1,10 @@
 from picamera2 import Picamera2
 
+OUTPUT_FOLDER = "/var/www/html/"
+
 cam = Picamera2()
 
-cam.start_and_capture_file("/var/www/html/snapshot.jpg")
+cam.start_and_capture_file(OUTPUT_FOLDER + "snapshot.jpg")
+
+print("Content-type: application/json")
+print({"file": "snapshot.json"})
