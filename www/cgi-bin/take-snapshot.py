0a8f16c352e7613939cdd6b1351e12486ddf856c
Carson Jones <djcubed12@gmail.com>
2024-04-11 17:47:33 -0500

    Fix take-snapshot.py

--- www/cgi-bin/take-snapshot.py
+++ www/cgi-bin/take-snapshot.py
@@ -1,11 +1,15 @@
 #!/bin/python3
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 
 OUTPUT_FOLDER = "/var/www/html/"
 
+# Apache CGI header
+print("Content-type: application/json")
+print()
+
 cam = Picamera2()
+cam.start_preview(Preview.NULL)
 
 cam.start_and_capture_file(OUTPUT_FOLDER + "snapshot.jpg")
 
-print("Content-type: application/json")
 print({"file": "snapshot.json"})
