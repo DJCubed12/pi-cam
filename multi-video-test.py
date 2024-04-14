48b1ece28fa3af5546305073ae12f483f767fa98
DJCubed12 <djcubed12@gmail.com>
2024-04-14 18:13:07 -0500

    2 videos 1 encoder

--- multi-video-test.py
+++ multi-video-test.py
@@ -14,29 +14,27 @@ cam.configure(config)
 
 mainOutput = FfmpegOutput("test-main.mp4")
 loresOutput = FfmpegOutput("test-lores.mp4")
-mainEncoder = H264Encoder()
-loresEncoder = H264Encoder()
+encoder = H264Encoder()
 
-cam.start_encoder(mainEncoder, name="main")
-cam.start_encoder(loresEncoder, name="lores")
+cam.start_encoder(encoder)  # main
 cam.start()
 
 print("Starting main video!")
 mainOutput.start()
-mainEncoder.output = mainOutput
+encoder.output = mainOutput
 
 input("> Enter to start lores video: ")
 loresOutput.start()
-loresEncoder.output = loresOutput
+encoder.output = [mainOutput, loresOutput]
 
 print(f"Lores video ends in {LENGTH} sec...")
 time.sleep(LENGTH)
-cam.stop_encoder(loresEncoder)
+encoder.output = mainOutput
 loresOutput.stop()
 print("Lores video stopped and saved")
 
 input("> Enter to stop main video: ")
-cam.stop_encoder(mainEncoder)
+cam.stop_encoder(encoder)
 mainOutput.stop()
 cam.stop()
 
